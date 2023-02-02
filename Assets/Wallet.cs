020c24273d992e3c640b963e45d29bfc29becead
Fernando de Paz <f.depaz@hotmail.com>
2023-02-01 19:13:25 -0800

    JUMPIESSSSSSSSSSSSSSS
    HEHE :)

--- Assets/Wallet.cs
+++ Assets/Wallet.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Wallet : MonoBehaviour
-{
+{.
+
+    public void collectcoins() {}
     // Start is called before the first frame update
     void Start()
     {
