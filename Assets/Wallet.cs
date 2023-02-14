29ae864150f2ef8f0dc3eb697486da33b1e7e08e
bytesandbots <students@bytesandbots.com>
2023-02-13 18:06:36 -0800

    LOLOLOP>L:PL UWID ak,mv
    LOLLOLOLOLOLOLO

--- Assets/Wallet.cs
+++ Assets/Wallet.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Wallet : MonoBehaviour
 {
-    public void collectcoins() {}
+    public float coins;
+    public TMP_Text cointext;
+
+    public void collectcoins(float newAmount ) {
+
+        coins += newAmount;
+        cointext.text = "Money have = " + coins.ToString() + ".mill";
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        cointext.text = "Money have = " + coins.ToString() + ".mill";
     }
 
     // Update is called once per frame
